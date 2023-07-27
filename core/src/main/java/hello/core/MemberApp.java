3d45e355748b82a73184cbc11c8a2d7c70568066
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-07-27 16:01:17 +0900

    스프링 핵심원리 기본편 - 스프링 컨테이너, 빈

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,18 @@ package hello.core;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 //        MemberService memberService = new MemberServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig에 있는 환경설정을 스프링 컨테이너에 넣어서 관리해줌
+        //ApplicationContext를 스프링 컨테이너라고 함, 또한 인터페이스임(다형성 적용)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Member.Grade.VIP);
         memberService.join(member);
 
---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,36 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer(){
+        AppConfig appConfig = new AppConfig();
+        //호출할 때 마다 객체를 생성
+        MemberService memberService = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService).isNotSameAs(memberService2);
+    }
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService instance = SingletonService.getInstance();
+        SingletonService instance1 = SingletonService.getInstance();
+
+        System.out.println("instance1 = " + instance1);
+        System.out.println("instance = " + instance);
+
+        Assertions.assertThat(instance).isSameAs(instance1);
+    }
+}
