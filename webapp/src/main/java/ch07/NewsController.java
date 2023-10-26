281a23cd873c66e5b439a31e07eb4f08fa7483bf
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-10-26 10:03:44 +0900

    FORM 공부

--- webapp/src/main/java/ch07/NewsController.java
+++ webapp/src/main/java/ch07/NewsController.java
@@ -12,16 +12,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet("/news.nhn")
-@MultipartConfig(maxFileSize = 1024*1024*2 , location = "C:/Temp/img")
+@MultipartConfig(maxFileSize = 1024*1024*2 , location = "C:\\spring_study\\jwbook\\src\\main\\webapp\\img")
 public class NewsController extends HttpServlet {
     private NewsDAO newsDAO;
     private ServletContext ctx;
-    private final String START_PAGE = "/ch07/newsList.jsp";
+    private final String START_PAGE = "/ch07/newsList.jsp"; // ?
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -32,30 +33,30 @@ public class NewsController extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("utf-8");
+//        req.setCharacterEncoding("utf-8");
         String action = req.getParameter("action");
         String view= null;
         Method m;
         System.out.println("action == " + action);
         if(action == null) {
-            action = "list";
+            ctx.getRequestDispatcher("/news.nhn?action=list").forward(req,resp);
         } else {
             try {
-                m = this.getClass().getMethod(action, HttpServletRequest.class, HttpServletResponse.class);
-                view = (String)m.invoke(this, req, resp);
+                m = this.getClass().getMethod(action, HttpServletRequest.class);
+                view = (String)m.invoke(this, req);
             } catch (Exception e){
                 e.printStackTrace();
             }
-        }
-        if(view.startsWith("redirect:/")){
-            view = view.substring("redirect:/".length());
-            resp.sendRedirect(view);
-        } else {
-            ctx.getRequestDispatcher("/ch07/"+ view).forward(req,resp);
+            if(view.startsWith("redirect:/")){
+                view = view.substring("redirect:/".length());
+                resp.sendRedirect(view);
+            } else {
+                ctx.getRequestDispatcher("/ch07/"+ view).forward(req,resp);
+            }
         }
     }
 
-    public String addNews(HttpServletRequest req, HttpServletResponse resp){
+    public String addNews(HttpServletRequest req){
         News news = new News();
         try {
             Part part = req.getPart("img");
@@ -66,34 +67,50 @@ public class NewsController extends HttpServlet {
                 part.write(fileName);
             }
             BeanUtils.populate(news, req.getParameterMap());
-            news.setImg("C:\\Temp\\img\\" + fileName);
+            news.setImg("/img/" + fileName);
             newsDAO.addNews(news);
         } catch (Exception e) {
             e.printStackTrace();
+            ctx.log("뉴스 등록 과정에서 문제 발생!");
+            req.setAttribute("error", "뉴스가 정상적으로 등록되지 않았습니다.");
+            return list(req);
         }
         return "redirect:/news.nhn?action=list";
     }
 
-    public String list(HttpServletRequest req, HttpServletResponse resp){
+    public String list(HttpServletRequest req){
+        String err = (String)req.getAttribute("error");
+        System.out.println("err == " + err);
         try {
             List<News> list = newsDAO.getAll();
-            req.setAttribute("newsList",list);
+            System.out.println("list ==" + list);
+            req.setAttribute("newsList", list);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
         return "newsList.jsp";
     }
 
-    public String delNews(HttpServletRequest req, HttpServletResponse resp){
-        return null;
+    public String delNews(HttpServletRequest req){
+        try {
+            newsDAO.delNews(Integer.parseInt(req.getParameter("aid")));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            ctx.log("뉴스를 삭제하는 과정에서 문제 발생!");
+            req.setAttribute("error", "뉴스를 정상적으로 삭제하지 못했습니다.");
+            return list(req);
+        }
+        return "redirect:/news.nhn?action=list";
     }
 
-    public String getNews(HttpServletRequest req, HttpServletResponse resp){
+    public String getNews(HttpServletRequest req){
         try {
-            News aid = newsDAO.getNews(Integer.parseInt(req.getParameter("aid")));
-            req.setAttribute("news",aid);
+            News news = newsDAO.getNews(Integer.parseInt(req.getParameter("aid")));
+            req.setAttribute("news", news);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            ctx.log("뉴스를 가져오는 과정에서 문제 발생!");
+            req.setAttribute("error", "뉴스를 정상적으로 가져오지 못했습니다.");
         }
         return "newsView.jsp";
     }
